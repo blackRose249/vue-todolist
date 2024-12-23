372fb139fba601b0ddae580ebd037b4ade4e8dad
huanghaijin <haijin.huang@bit-jp.co.jp>
2024-12-23 18:07:41 +0900

    AOP機能の追加
    Castle.DynamicProxyを使用して実現する

---
+++ WebApplication1/Interceptor/MyInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using Castle.DynamicProxy;
+
+namespace WebApplication1.Interceptor
+{
+    public class MyInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine($"Method {invocation.Method.Name} is called;Staring!");
+            invocation.Proceed();
+            Console.WriteLine($"Method {invocation.Method.Name} is called;Ending");
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,8 +1,12 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using WebApplication1.Contexts;
 using WebApplication1.Repository;
 using WebApplication1.Services;
+using WebApplication1.Interceptor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,8 +34,24 @@ builder.Services.AddDbContext<TodoLIstDbContext>(options =>
     options.UseMySql("server=localhost;port=3306;database=TodoList_Root;uid=root;pwd=011007;CharSet=utf8",
     new MySqlServerVersion("5.7.44"));
 });
-builder.Services.AddScoped<ITodoRepository, TodoRepository>();
-builder.Services.AddScoped<ITodoListService, TodoListService>();
+// when requesing,new Instance;
+//builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+//builder.Services.AddScoped<ITodoListService, TodoListService>();
+
+// 1,use autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+// 2,reg autofac's service containers 
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    // TypeÅFSingleInstance()ÅAInstancePerLifetimeScope()ÅAInstancePerDependency()
+    //containerBuilder.RegisterType<TodoListService>().As<ITodoListService>().SingleInstance();
+    //containerBuilder.RegisterType<TodoRepository>().As<ITodoRepository>().SingleInstance();
+    containerBuilder.RegisterType<TodoListService>().As<ITodoListService>().EnableInterfaceInterceptors().InterceptedBy(typeof(MyInterceptor));
+    containerBuilder.RegisterType<TodoRepository>().As<ITodoRepository>().EnableInterfaceInterceptors().InterceptedBy(typeof(MyInterceptor));
+    // reg interceptor
+    containerBuilder.RegisterType<MyInterceptor>();
+});
+
 
 var app = builder.Build();
 
